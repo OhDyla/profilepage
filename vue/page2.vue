f638036c3b2f64e27ef02168b985edd2c0ef3e29
KenOHD <al19078@shibaura-it.ac.jp>
2021-10-06 01:52:12 +0900

    add_page2

--- vue/page2.vue
+++ vue/page2.vue
@@ -3,9 +3,8 @@
   <Hero></Hero>
   <Jumper activater=2></Jumper>
   <div class="who">
-    <div class="title">Who is ...</div>
     <div class="who__title-box">
-      <img>
+      <img class="who__title-img">
       <div class="who__title-title">自己紹介</div>
     </div>
     <div class="who__details">
@@ -13,60 +12,71 @@
         新潟県新潟市出身
       </div>
       <div class="who__detail">
-        芝浦工業大学&nbsp;工学部情報工学科に在学中。<br>
-        23卒。大学院進学予定はありません。
+        芝浦工業大学&nbsp;工学部情報工学科に在学中。23卒。大学院進学予定なし。
       </div>
       <div class="who__detail">
-        大学ではCSと数値解析を専攻。<br>その傍ら弊学大学祭「芝浦祭」の実行委員会に所属。2020年11月より第49回芝浦祭実行委員長に就任。
+        学部ではCSと数値解析を専攻。
       </div>
       <div class="who__detail">
-        業務委託エンジニアとして、Web系開発会社の株式会社エニーアップに就業中。<br>
-        業務歴約1年半。主にフロント側が Vue3 + TypeScript(/JS)、バックが Go + Revel を使用。
+        その傍ら弊学大学祭「芝浦祭」の実行委員会に所属。2020年11月より第49回芝浦祭実行委員長に就任。
+      </div>
+      <div class="who__detail">
+        Web系開発会社に就業中。<br>業務歴約1年半。主にフロント側が Vue3 + TypeScript (/JS) バック側が Go + Revel を使用。
       </div>
     </div>
 
     <div class="who__title-box">
-      <img>
+      <img class="who__title-img">
       <div class="who__title-title">情報工学科での活動</div>
     </div>
+    <div class="who__details">
+      <div class="who__detail">
+        芝浦工業大学工学部情報工学科在学。<br>現在学部3年。
+      </div>
+      <div class="who__detail">
+        コンピュータサイエンス(CS)と数値計算を学修中。
+      </div>
+      <div class="who__detail">
+        主な使用言語は<br>
+        { メイン : C言語,<br>
+        &nbsp;&nbsp;数値画像解析系 : Python3,<br>
+        &nbsp;&nbsp;アプリケーション系 : Java }<br>
+        その他、ShellScriptやアセンブリ言語なども使用。
+      </div>
+    </div>
 
     <div class="who__title-box">
-      <img>
+      <img class="who__title-img">
       <div class="who__title-title">芝浦祭実行委員会での活動</div>
     </div>
-
-
-
-    <div class="who__box">
-      <div class="who__box-title"><span>自己紹介</span></div>
-      <div class="who__box-detail">
-        新潟県新潟市出身。<br>
-        芝浦工業大学&nbsp;工学部情報工学科在学<br>23卒。大学院進学予定はありません
-        <div class="null-space"></div>
-        大学ではCSと数値解析を専攻し、その傍ら弊学大学祭「芝浦祭」の実行委員会に所属<br>
-        2020年11月より<span class="underline r">第49回芝浦祭実行委員長</span>に就任。
-        <div class="null-space"></div>
-        業務委託エンジニアとして、Web系開発会社の株式会社エニーアップにて<span class="underline b">フロント/バックエンドエンジニア</span>として就業中。業務歴約1年半。主にフロント側が Vue3 + TypeScript(/JS)、バックが Go + Revel を使用。
+    <div class="who__details">
+      <div class="who__detail">
+        第49回芝浦祭実行委員会実行委員長
+      </div>
+      <div class="who__detail">
+        広報局Web部門出身。元Web部門長
+      </div>
+      <div class="who__detail">
+        2021年11月5(Fri)〜7(Sun)日、第49回芝浦祭開催。<br>
+        キャンパス開催 (在学生のみ対象) およびオンライン開催によるハイブリッド形式。<br>
+      </div>
+      <div class="who__detail">
+        ぜひ見てね。
       </div>
     </div>
-    <div class="who__box">
-      <div class="who__box-title">情報工学科での活動</div>
-      <div class="who__box-detail">
-        芝浦工業大学&nbsp;工学部情報工学科在学。現在学部3年。23卒。大学院進学予定なし(0%)<br>
-        コンピュータサイエンス(CS)と数値計算を学修中。主な使用言語は、メインがC言語、数値画像解析系でPython3、アプリケーション系でJava。その他、ShellScriptやアセンブリ言語なども使用。
-      </div>
-    </div>
-    <div class="who__box">
-      <div class="who__box-title">芝浦祭での活動</div>
-      <div class="who__box-detail">
 
-      </div>
+    <div class="who__title-box">
+      <img class="who__title-img">
+      <div class="who__title-title">エンジニアとしての活動</div>
     </div>
-    <div class="who__box">
-      <div class="who__box-title">エンジニアとしての活動</div>
-      <div class="who__box-detail">
-        <router-link tag="div" to="/page3" class="">詳細はこちら</router-link>
+    <div class="who__details">
+      <div class="who__detail">
+        主な活動内容は以下を参照
       </div>
+    <router-link tag="div" to="/page3" class="who__detail is-link">
+      <i class="fas fa-link who__detail-icon"></i>
+      JUMP!
+    </router-link>
     </div>
   </div>
 </div>
@@ -93,7 +103,19 @@ module.exports = {
 }
 
 .who__title-box {
+  display: flex;
+  align-items: center;
   margin-bottom: 8px;
+}
+
+.who__title-img {
+  margin-right: 8px;
+  width: 28px;
+  height: 28px;
+  /* あとで消す↓ */
+  border: 1px solid #DDDDDD;
+  /* あとで消す↑ */
+  border-radius: 50%;
 }
 
 .who__title-title {
@@ -108,13 +130,14 @@ module.exports = {
   display: flex;
   flex-direction: column;
   align-items: flex-end;
+  margin-bottom: 28px;
 }
 
 .who__detail {
   display: inline-block;
   margin-bottom: 2px;
   padding: 8px 12px;
-  max-width: 300px;
+  max-width: 270px;
   font-size: 14px;
   line-height: 20px;
   color: #FFFFFF;
@@ -127,41 +150,18 @@ module.exports = {
 }
 
 .who__detail:last-child {
-  margin-bottom: 12px;
   border-bottom-right-radius: 16px;
 }
 
-.who__box {
-  margin-bottom: 52px;
-  width: 80%;
+.who__detail.is-link {
+  font-size: 18px;
+  font-weight: bold;
+  color: #DDDDDD;
+  line-height: 24px;
 }
 
-.who__box:nth-child(2n+1) {
-    margin-left: auto;
-}
-
-.who__box-title {
-  margin-bottom: 8px;
-  padding-bottom: 4px;
-  display: inline-block;
-  font-size: 16px;
-  border-bottom: 4px double #FFFFFF;
-}
-
-.who__box-detail {
-  font-size: 14px;
-  line-height: 20px;
-}
-
-.null-space {
-  height: 10px;
-}
-
-.underline.r {
-  background: linear-gradient(transparent 60%, #FF6347 0%);
-}
-.underline.b {
-  background: linear-gradient(transparent 60%, #1E90FF 0%);
+.who__detail-icon {
+  font-size: 22px;
 }
 
 </style>
