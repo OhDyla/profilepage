8c9fbc1f1ac3227a69669f7f8894e5c33dea6548
KenOHD <al19078@shibaura-it.ac.jp>
2021-10-07 03:24:06 +0900

    add_history-photo

--- vue/history.vue
+++ vue/history.vue
@@ -6,12 +6,16 @@
       <div class="history__year">2021</div>
       <div class="history__box-wrapeer">
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <div class="history__box-photo is-null"></div>
+          </div>
           <div class="hisotory__box-detail">ワクチン2回目を接種しました。<span class="hisotory__box-detail-month">8月</span></div>
           <div class="hisotory__box-tag is-deactive">その他</div>
         </div>
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <img src="../static/history/koto.jpg" class="history__box-photo">
+          </div>
           <div class="hisotory__box-detail">東京都江東区に移住しました。<span class="hisotory__box-detail-month">2月</span></div>
           <div class="hisotory__box-tag is-deactive">移住</div>
         </div>
@@ -21,12 +25,16 @@
       <div class="history__year">2020</div>
       <div class="history__box-wrapeer">
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <img src="../static/history/shibasai49.jpg" class="history__box-photo">
+          </div>
           <div class="hisotory__box-detail">芝浦祭実行委員会<span class="history__bold">第49回実行委員長</span>に就任しました。<span class="hisotory__box-detail-month">11月</span></div>
           <div class="hisotory__box-tag is-deactive">課外活動</div>
         </div>
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <div class="history__box-photo is-null"></div>
+          </div>
           <div class="hisotory__box-detail"><span class="history__bold">株式会社エニーアップ</span>に就業しました。<span class="hisotory__box-detail-month">9月</span></div>
           <div class="hisotory__box-tag">業務</div>
         </div>
@@ -36,17 +44,23 @@
       <div class="history__year">2019</div>
       <div class="history__box-wrapeer">
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <img src="../static/history/shibasai47.jpg" class="history__box-photo">
+          </div>
           <div class="hisotory__box-detail">芝浦祭実行委員会広報局Web部門長, 天文同好会企画役員, 国際交流サークルSISA幹部に就任しました。<span class="hisotory__box-detail-month">11月</span></div>
           <div class="hisotory__box-tag is-deactive">課外活動</div>
         </div>
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <img src="../static/history/astro.jpg" class="history__box-photo">
+          </div>
           <div class="hisotory__box-detail">芝浦祭実行委員会, 天文同好会, 国際交流サークルSISAに入部しました。<span class="hisotory__box-detail-month">4月</span></div>
           <div class="hisotory__box-tag is-deactive">課外活動</div>
         </div>
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <img src="../static/history/sit-enter.jpg" class="history__box-photo">
+          </div>
           <div class="hisotory__box-detail"><span class="history__bold">芝浦工業大学&nbsp;工学部情報工学科</span>に入学しました。<span class="hisotory__box-detail-month">4月</span></div>
           <div class="hisotory__box-tag">入学</div>
         </div>
@@ -56,7 +70,9 @@
       <div class="history__year">2018</div>
       <div class="history__box-wrapeer">
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <img src="../static/history/saitama.jpg" class="history__box-photo">
+          </div>
           <div class="hisotory__box-detail">埼玉県さいたま市に移住しました。<span class="hisotory__box-detail-month">4月</span></div>
           <div class="hisotory__box-tag is-deactive">移住</div>
         </div>
@@ -66,7 +82,9 @@
       <div class="history__year">2016</div>
       <div class="history__box-wrapeer">
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <div class="history__box-photo is-null"></div>
+          </div>
           <div class="hisotory__box-detail">文系から理系になりました。<span class="hisotory__box-detail-month">4月</span></div>
           <div class="hisotory__box-tag is-deactive">学業</div>
         </div>
@@ -76,12 +94,16 @@
       <div class="history__year">2015</div>
       <div class="history__box-wrapeer">
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <div class="history__box-photo is-null"></div>
+          </div>
           <div class="hisotory__box-detail">東京都北区に移住しました。<span class="hisotory__box-detail-month">4月</span></div>
           <div class="hisotory__box-tag is-deactive">移住</div>
         </div>
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <div class="history__box-photo is-null"></div>
+          </div>
           <div class="hisotory__box-detail"><span class="history__bold">新潟県立新潟高等学校</span>を卒業しました。<span class="hisotory__box-detail-month">3月</span></div>
           <div class="hisotory__box-tag">修業</div>
         </div>
@@ -91,12 +113,16 @@
       <div class="history__year">2012</div>
       <div class="history__box-wrapeer">
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <div class="history__box-photo is-null"></div>
+          </div>
           <div class="hisotory__box-detail">硬式テニス部, クイズ研究会に入部しました。<span class="hisotory__box-detail-month">4月</span></div>
           <div class="hisotory__box-tag is-deactive">課外活動</div>
         </div>
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <div class="history__box-photo is-null"></div>
+          </div>
           <div class="hisotory__box-detail"><span class="history__bold">新潟県立新潟高等学校</span>に入学しました。<span class="hisotory__box-detail-month">4月</span></div>
           <div class="hisotory__box-tag">入学</div>
         </div>
@@ -106,12 +132,16 @@
       <div class="history__year">2008</div>
       <div class="history__box-wrapeer">
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <div class="history__box-photo is-null"></div>
+          </div>
           <div class="hisotory__box-detail"><span class="history__bold">RoboCup2008&nbsp;JapanOpen Osaka</span>&nbsp;にてジュニア部門第5位受賞。</div>
           <div class="hisotory__box-tag is-deactive">獲得</div>
         </div>
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <div class="history__box-photo is-null"></div>
+          </div>
           <div class="hisotory__box-detail"><span class="history__bold">RoboCup2008&nbsp;北信越大会</span>&nbsp;にてジュニア部門優勝。</div>
           <div class="hisotory__box-tag is-deactive">獲得</div>
         </div>
@@ -121,7 +151,9 @@
       <div class="history__year">A long time ago....</div>
       <div class="history__box-wrapeer">
         <div class="history__box">
-          <div class="history__box-photo-wrapper"></div>
+          <div class="history__box-photo-wrapper">
+            <img src="../static/history/niigata.jpg" class="history__box-photo">
+          </div>
           <div class="hisotory__box-detail">新潟県新潟市出身。</div>
           <div class="hisotory__box-tag is-deactive">出身</div>
         </div>
@@ -171,6 +203,21 @@ module.exports = {
 .history__box-photo-wrapper {
   flex: 0 0 15%;
 }
+
+.history__box-photo {
+  width: 44px;
+  height: auto;
+  border-radius: 50%;
+}
+
+.history__box-photo.is-null {
+  width: 44px;
+  height: 44px;
+  background-color: #AAAAAA;
+  border: 2px solid #CCCCCC;
+  border-radius: 50%;
+}
+
 
 .hisotory__box-detail {
   flex: 0 0 60%;
